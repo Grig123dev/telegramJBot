47725e78f6de494ed6bdc5ac4e89f4136cd550c0
Grig123dev <grigorisargsyan01@gmail.com>
2020-09-25 18:49:45 +0400

    Updates

---
+++ precache-manifest.11ba04ea507c5fae97d2414f28962a75.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ec69ca9511aaead919dc85f086286d7",
+    "url": "/botTelegram/index.html"
+  },
+  {
+    "revision": "27998a587f1e490a1c0d",
+    "url": "/botTelegram/static/css/main.8a57c41f.chunk.css"
+  },
+  {
+    "revision": "e5335b537547614a1cb2",
+    "url": "/botTelegram/static/js/2.2d40e1a3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/botTelegram/static/js/2.2d40e1a3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27998a587f1e490a1c0d",
+    "url": "/botTelegram/static/js/main.87f25bf2.chunk.js"
+  },
+  {
+    "revision": "d03b67f4fef44537e730",
+    "url": "/botTelegram/static/js/runtime-main.355592e3.js"
+  }
+]);
